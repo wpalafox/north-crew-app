f2dd087cbe3d734e7d773ba9e7cb092b28cb9237
wpalafox <wpalafox@gmail.com>
2021-12-31 15:23:47 -0700

    races update

--- src/Races.js
+++ src/Races.js
@@ -4,9 +4,9 @@ import './Races.css'
 export default function Races() {
     
     const data = [
-        { race: 'Franklin Mountains | Stop #3', link: "/franklin_mountains_2021" },
-        { race: 'Lost Dog | Stop #2', link: "/lost_dog_2021"},
-        { race: 'Monks | Stop #1', link: "/monks_2021" },
+        { race: 'Franklin Mountains | 12/18/2021 ', link: "/franklin_mountains_2021" },
+        { race: 'Lost Dog | 11/21/2021 ', link: "/lost_dog_2021"},
+        { race: 'Monks | 10/23/2021', link: "/monks_2021" },
       ];
     
       const race_links = data.map((item) =>
