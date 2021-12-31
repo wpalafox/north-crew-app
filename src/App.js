fe0841e7769c32b3e0d7d951976effd36fcca051
wpalafox <wpalafox@gmail.com>
2021-12-31 10:54:22 -0700

    home page update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component }  from 'react';
 import './App.css';
 
 import ep_logo from './ep_enduro_logo.svg';
+import hero_image from './pics/home_image_stoked.jpg';
 import background_ep from './pics/ep_enduro_background.jpg';
 
 
@@ -10,32 +11,34 @@ import background_ep from './pics/ep_enduro_background.jpg';
 function App() {
   return (
     <div className="App">
-   
-      <header className="App-header">
-        <div className='container'>
-        <img src={ep_logo} className="App-logo pulse" alt="logo" />
+       <img id="hero-image" src={hero_image} alt="ep mountain" />
+        <header className="App-header">
+          <div className='container'>
         
-        <p>
-        An Organization Founded and Supported by Local Mountain Bike Riders.
-        </p>
-        <p>
-        We Promote and Organize MTB Enduro Style Race Events, and Trail Maintenance Activities.
-        </p>
+          
+          <img src={ep_logo} className="App-logo pulse" alt="logo" />
+          
+          <p>
+          An Organization Founded and Supported by Local Mountain Bike Riders.
+          </p>
+          <p>
+          We Promote and Organize MTB Enduro Style Race Events, and Trail Maintenance Activities.
+          </p>
+        
       
-     
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=wWH_BSRiX0U"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          What Is Enduro MTB Racing?
-        </a>
-        </div>
-      </header>
-      <img id="home-image" src={background_ep} alt="ep mountain" />
+          <a
+            className="App-link"
+            href="https://www.youtube.com/watch?v=wWH_BSRiX0U"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            What Is Enduro MTB Racing?
+          </a>
+          </div>
+        </header>
+        <img id="home-image" src={background_ep} alt="ep mountain" />
 
-  
+    
       
     </div>
   );
