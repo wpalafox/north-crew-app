1074663e85099d53ffb0be5b31f16f73b2137885
wpalafox <wpalafox@gmail.com>
2022-01-05 12:06:06 -0700

    added logo big back

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
           <div className='container'>
         
           
-          
+          <img src={ep_logo} className="App-logo pulse" alt="logo" />
           
               <p className='introduction'>
               The EP Enduro series features a variety of Enduro style mountain bike racing in El Paso, Texas. 
