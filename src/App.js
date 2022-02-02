9795e25983ebb7f0b150ddd1dc97f6fd3826d4d1
wpalafox <wpalafox@gmail.com>
2022-02-02 11:47:33 -0700

    news update

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import pablo_hustle from './pics/pablo_hustling.jpg';
 function App() {
   
   const data = [
+    { news: 'Gnarliest trails in El Paso', link: "https://www.instagram.com/p/CZe4Y3NsUVG/", caption: 'get your skills to the next level 🆙 join us for a whole weekend of fun, jumps, drops and steep trails 🤘🏻🤘🏻 Grab your spot 👉 https://www.bikereg.com/rrbash $10 camping $5 parking to fund Mountain Action Sports El Paso' },
     { news: 'Check this run!', link: "https://www.facebook.com/epduro/videos/4952165258181682/", caption: 'Check this run on part of one of the stages for ROUGHRIDER BASH, EP ENDURO 4 🤘🏻🤘🏻 Getting closer!! Go to bike reg and grab your spot 🤙🏻🤙🏻 Video Courtesy of Mad Max little shredder 👊🏻' },
     { news: 'Get ready to take your riding to the next level!', link: "https://www.instagram.com/p/CZbBCDIloa_/", caption: 'Register for stop 4 🛑 roughrider bash Feb 18-19-20, full weekend of shredding 🤘🏻🤘🏻 $10 camping and $5 parking to benefit Mountain Action Sports El Paso 🤙🏻🤙🏻' },
     { news: 'Congratulations to all the participants and the winners', link: "https://www.facebook.com/epduro/posts/677340323636093", caption: 'Winners are 72. Ricky Rod - merol Racing Hat 11. Julio Terrazas - free basic tune up at Podium 16.Norma Rivera. Evil Insurgent LB bike 🤘🏻🤘🏻' },
