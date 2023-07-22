58f033203a5d07629f53bba3324326be18782b64
William Palafox <wpalafox@gmail.com>
2023-07-21 21:22:15 -0400

    benita

--- src/NotFound.js
+++ src/NotFound.js
@@ -5,7 +5,7 @@ import bendita from './pics/Bendita.jpg';
 export default function NotFound() {
     return (
         <div className='notfound'>
-             <h1 className='victory'>Bendita's Page</h1>
+             <h1 className='victory'>Not Found so here's Benita's Page</h1>
             <img id="bendita-image" src={bendita} alt="dog" />
             
         </div>
